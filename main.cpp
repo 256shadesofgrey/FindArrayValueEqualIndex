f5ef760aa001d6133d19daa9c234367a27db9cbf
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-11-12 05:42:08 +0100

    Fix issues with the CLI

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <string>
+
+#include <sys/select.h>
+
 #include <TestArray.hpp>
 #include <BinarySearch.hpp>
 
 using namespace std;
 
 // "./FindArrayValueEqualIndex" uses a randomly generated array.
-// "./FindArrayValueEqualIndex <n1> ..." uses numbers from stdin.
+// "echo <n1> ... | ./FindArrayValueEqualIndex" will use piped values for the array.
+// "./FindArrayValueEqualIndex <n1> ..." uses numbers passed as parameters as
+//     the array.
 // "./FindArrayValueEqualIndex set <len> <startVal> <minStep> <maxStep>" uses
 //     a randomly generated array with the given parameters.
 int main(int argc, char *argv[]){
@@ -14,17 +19,42 @@ int main(int argc, char *argv[]){
 
   // TODO: implement getopt interface.
   // TODO: check if inputs are correct.
-  if(argc <= 1){
-    cout<<"Testing with randomly generated array."<<endl;
-    ta = new TestArray();
-  }else{
-    if(isdigit(argv[0][0])){
-      cout<<"Testing with array from stdin."<<endl;
+  if(argc > 1){
+    bool firstParamInteger = true;
+
+    try{
+      stoi(argv[1]);
+    }catch(invalid_argument){
+      firstParamInteger = false;
+    }
+
+    if(firstParamInteger){
+      cout<<"Testing with array passed as parameters."<<endl;
       ta = new TestArray(argc-1, &argv[1]);
     }else{
       cout<<"Testing with randomly generated array with given parameters"<<endl;
       ta = new TestArray(stoi(argv[2]), stoi(argv[3]),
                          stoi(argv[4]), stoi(argv[5]));
+    }
+  }else{
+    int32_t n;
+    vector<int32_t> buffer;
+
+    int fd_stdin = fileno(stdin);
+    fd_set readfds;
+    FD_SET(fd_stdin, &readfds);
+    struct timeval tv = {0, 0};
+
+    while(select(1, &readfds, NULL, NULL, &tv) && cin>>n){
+      buffer.push_back(n);
+    }
+
+    if(buffer.size() > 0){
+      cout<<"Testing with piped values."<<endl;
+      ta = new TestArray(buffer);
+    }else{
+      cout<<"Testing with randomly generated array."<<endl;
+      ta = new TestArray();
     }
   }
 
@@ -37,4 +67,6 @@ int main(int argc, char *argv[]){
   }else{
     cout<<"There is no value that equals its index."<<endl;
   }
+
+  return 0;
 }
