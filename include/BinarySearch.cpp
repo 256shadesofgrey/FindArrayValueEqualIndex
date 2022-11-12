23fb1b8e713e4bb1c4d521fe79d932ef39de6cc5
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-11-12 02:14:03 +0100

    Implement the search

--- include/BinarySearch.cpp
+++ include/BinarySearch.cpp
@@ -1,2 +1,51 @@
 #include "BinarySearch.hpp"
 
+int32_t BinarySearch::search(const int32_t *array, uint32_t len)
+{
+  // Slope of value graph fv is mv>=1, while the slope of the index graph fi is mi=1.
+  // That means if fv starts above 0 or ends before len-1, there cannot possibly
+  // be an intersection that we're looking for, so we can skip the search.
+  if(array[0] > 0){
+    return -1;
+  }
+
+  // Subtracting 1 from len now to save one subtraction operation in the if stetement.
+  --len;
+
+  if(array[len] < len){
+    return -1;
+  }
+
+  // Adding 1 to len again because we need the original value for the actual
+  // search. This means that when the if statement above evaluates to false,
+  // we will not have saved any operations by doing it this way, but in cases
+  // when it evaluates to true, we will have saved one, so it's an average case
+  // improvement.
+  ++len;
+
+  uint32_t start = 0;
+
+  // Declaring variable outside of the scope of the loop, even though it is
+  // not needed outside, to make sure that the memory for it is only allocated
+  // once, which depending on compiler optimizations might save a few cycles.
+  // Not assigning a value to mid, because we will do it in the loop before it
+  // will be used the first time. This saves us one operation.
+  uint32_t mid;
+
+  // Doing the search iteratively to save memory on the stack.
+  while(start != len){
+    // We have to divide an integer by 2, which in binary gives the same result
+    // as shifting by 1 to the right. Depending on compiler optimizations and the
+    // CPUs this code will run on, this operation might be faster than division.
+    mid = (start + len) >> 1;
+    if(array[mid] == mid){
+      return mid;
+    }else if(array[mid] > mid){ // Intersection to the left of mid.
+      len = mid;
+    }else{ // Intersection to the right of mid.
+      start = mid+1;
+    }
+  }
+
+  return -1;
+}
--- include/BinarySearch.hpp
+++ include/BinarySearch.hpp
@@ -1,8 +1,11 @@
 #ifndef BINARY_SEARCH_HPP
 #define BINARY_SEARCH_HPP
 
-class BinarySearch{
+#include <cstdint>
 
+class BinarySearch{
+public:
+  static int32_t search(const int32_t *array, uint32_t len);
 };
 
 #endif // BINARY_SEARCH_HPP
